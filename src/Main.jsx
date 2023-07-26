b0fdd38a2b59e696209038687231160c0c879fe3
chrisptran <christopher.p.tran@gmail.com>
2023-07-25 23:48:40 -0700

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am in Main.jsx </h1>
+            </div>
+        )
+    }
+}
+
+export default Main
